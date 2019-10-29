5261eee64c088113a7c972dc61d2d14c35070c34
MinKyeongTae <kyeongtea92@gmail.com>
2019-10-29 19:26:07 +0900

    Add Initial Overall SignUp Interfaces -> Ready To Adjust signUp Functions

--- DeepFashion/FirstSignUpViewController.swift
+++ DeepFashion/FirstSignUpViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class FirstSignUpViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
---
+++ DeepFashion/SecondSignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondSignUpViewController.swift
+//  DeepFashion
+//
+//  Created by MinKyeongTae on 29/10/2019.
+//  Copyright © 2019 MinKyeongTae. All rights reserved.
+//
+
+import UIKit
+
+class SecondSignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
