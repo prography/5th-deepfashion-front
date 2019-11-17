5b41c0b5247d03d66422cf23f000b6d72ae00696
MinKyeongTae <kyeongtea92@gmail.com>
2019-11-17 19:02:17 +0900

    Fix Style Indexing Error by Wrong Spelling Index Name

--- DeepFashion/Model/SingleTon/CommonUserData.swift
+++ DeepFashion/Model/SingleTon/CommonUserData.swift
@@ -15,7 +15,7 @@ final class CommonUserData {
     private(set) var id: String = ""
     private(set) var password: String = ""
     private(set) var gender: Int = 0
-    private(set) var selectedStyle = ["Casual": 0, "Formal": 0, "Street": 0, "Vintage": 0, "Hiphop": 0, "Sporty": 0, "Lovely": 0, "Luxuty": 0, "Sexy": 0, "Modern": 0, "Chic": 0, "Purity": 0, "Dandy": 0]
+    private(set) var selectedStyle = ["Casual": 0, "Formal": 0, "Street": 0, "Vintage": 0, "Hiphop": 0, "Sporty": 0, "Lovely": 0, "Luxury": 0, "Sexy": 0, "Modern": 0, "Chic": 0, "Purity": 0, "Dandy": 0]
 
     private init() {}
 
@@ -37,7 +37,7 @@ final class CommonUserData {
     }
 
     func resetStyleData() {
-        selectedStyle = ["Casual": 0, "Formal": 0, "Street": 0, "Vintage": 0, "Hiphop": 0, "Sporty": 0, "Lovely": 0, "Luxuty": 0, "Sexy": 0, "Modern": 0, "Chic": 0, "Purity": 0, "Dandy": 0]
+        selectedStyle = ["Casual": 0, "Formal": 0, "Street": 0, "Vintage": 0, "Hiphop": 0, "Sporty": 0, "Lovely": 0, "Luxury": 0, "Sexy": 0, "Modern": 0, "Chic": 0, "Purity": 0, "Dandy": 0]
         userData?.configureStyle(styles: selectedStyle)
     }
 }
