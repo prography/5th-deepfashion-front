7a1766d02c9399718958cb30ddb9669bef728031
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-27 23:49:27 +0900

    Add Initial IB Layout in DeleteUserViewController

---
+++ DeepFashion/Controller/Main/MainTabBarView/MyPageViewController/DeleteUserViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DeleteUserViewController.swift
+//  DeepFashion
+//
+//  Created by MinKyeongTae on 2019/12/27.
+//  Copyright © 2019 MinKyeongTae. All rights reserved.
+//
+
+import UIKit
+
+class DeleteUserViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewController()
+    }
+}
+
+extension DeleteUserViewController: UIViewControllerSetting {
+    func configureViewController() {}
+}
