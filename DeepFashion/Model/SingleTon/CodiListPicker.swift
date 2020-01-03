91d14e22c645dc89a522f3db5c2edf87fbcc6fd9
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-03 14:54:41 +0900

    Add File CodiListPicker

---
+++ DeepFashion/Model/SingleTon/CodiListPicker.swift
@@ -0,0 +1,15 @@
+//
+//  CodiListPicker.swift
+//  DeepFashion
+//
+//  Created by MinKyeongTae on 2020/01/03.
+//  Copyright © 2020 MinKyeongTae. All rights reserved.
+//
+
+import Foundation
+
+final class CodiListPicker {
+    static let shared = CodiListPicker()
+
+    private init() {}
+}
