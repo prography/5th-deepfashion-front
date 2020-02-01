e7aade72cf71ed06ff4185a1307d70adb8cf094d
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-01 16:38:43 +0900

    Add File, UserAPIPutData

---
+++ Fash/Model/Network/EditUserData/UserAPIPutData.swift
@@ -0,0 +1,22 @@
+//
+//  UserAPIPutData.swift
+//  Fash
+//
+//  Created by MinKyeongTae on 2020/02/01.
+//  Copyright © 2020 MinKyeongTae. All rights reserved.
+//
+
+import Foundation
+
+/// API UserData Format
+struct UserAPIPutData: Codable {
+    let userName: String
+    let gender: String
+    let styles: [Int]
+    let password: String
+
+    enum CodingKeys: String, CodingKey {
+        case userName = "username"
+        case gender, styles, password
+    }
+}
